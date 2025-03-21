92c1422ff6c09217dc50d78e747c3d193790f65c
Amit Hassan Joy <sarkeramit88@gmail.com>
2025-03-21 22:27:45 +0600

    fix navigation issue

--- frontend/uddhar-frontend/src/App.jsx
+++ frontend/uddhar-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import {Navbar, Landing} from "./public/Public";
 import Login from "./authentication/pages/Login";
-import ForgetPass from "./authentication/ForgetPass";
+import ForgetPass from "./authentication/pages/ForgetPass";
 
 const App = () => {
   return (
