0360816cac9e57df823db2d351411317ab9dea1c
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-03-24 23:26:05 +0600

    add validation middleware

--- backend/src/middlewares/validationMIddleware.js
+++ backend/src/middlewares/validationMIddleware.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator');
-const userRegistrationSchema = require('../validation/userValidation');
+const { userRegistrationSchema, userLoginSchema } = require('../validation/userValidation');
 
 
 const validateUserRegistration = async (req, res, next) => {
@@ -14,4 +14,17 @@ const validateUserRegistration = async (req, res, next) => {
   }
 };
 
-module.exports = { validateUserRegistration };
+
+const validateUserLogin = async (req, res, next) => {
+  try {
+    const { error } = userLoginSchema.validate(req.body);
+    if (error) {
+      return res.status(422).json({ message: error.details[0].message });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: 'Validation error occurred' });
+  }
+};
+
+module.exports = { validateUserRegistration, validateUserLogin };
