8a15219446035fd2b11c0900e038c32fa508d0db
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-03-24 23:17:20 +0600

    add user login validation

--- backend/src/validation/userValidation.js
+++ backend/src/validation/userValidation.js
@@ -2,11 +2,16 @@ const Joi = require('joi');
 
 const userRegistrationSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().regex(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/).required(),
+  email: Joi.string().email().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).required(),
   mobile: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
   password: Joi.string().min(8).required(),
   location: Joi.string().min(3).max(50).required(),
   role: Joi.string().valid('volunteer', 'organization', 'coordinator').required(),
 });
 
-module.exports = userRegistrationSchema;
+const userLoginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = { userRegistrationSchema, userLoginSchema };
