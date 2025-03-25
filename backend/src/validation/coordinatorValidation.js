096471a7fcd901da469113d5968fab9f6c8d8adb
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-03-25 23:16:19 +0600

    add coordinator validator

---
+++ backend/src/validation/coordinatorValidation.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const createDisasterSchema = Joi.object({
+  title: Joi.string().min(3).max(100).required(),
+  description: Joi.string().min(10).required(),
+  location: Joi.string().min(3).max(100).required(),
+  startDate: Joi.date().required()
+});
+
+const assignDisasterToTeamSchema = Joi.object({
+  teamId: Joi.number().integer().required(),
+  disasterId: Joi.number().integer().required()
+});
+
+const emergencyNotificationSchema = Joi.object({
+  subject: Joi.string().min(3).max(100).required(),
+  message: Joi.string().min(1).required()
+});
+
+module.exports = {
+  createDisasterSchema,
+  assignDisasterToTeamSchema,
+  emergencyNotificationSchema
+};
