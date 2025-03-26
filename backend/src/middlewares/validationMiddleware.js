876c61585fe7f8d8d05c9a8a2dc2f9862abbd65e
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-03-26 13:00:32 +0600

    fix: add generic validation middleware

---
+++ backend/src/middlewares/validationMiddleware.js
@@ -0,0 +1,11 @@
+const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(422).json({ message: error.details[0].message });
+    }
+    next();
+  };
+};
+
+module.exports = { validateRequest };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { register, login, verifyEmail, requestForgotPassword, resetPassword } = require('../controllers/authController');
-const { validateUserRegistration, validateUserLogin, validatePasswordReset } = require('../middlewares/validationMIddleware');
+const { validateRequest } = require('../middlewares/validationMiddleware');
+const { userRegistrationSchema, userLoginSchema, resetPasswordSchema } = require('../validation/userValidation');
 
 
 const router = express.Router();
 
-router.post("/register", validateUserRegistration, register);
-router.post("/login", validateUserLogin, login);
+router.post('/register', validateRequest(userRegistrationSchema), register);
+router.post('/login', validateRequest(userLoginSchema), login);
 
 router.post("/verify-email", verifyEmail);
 
 router.post("/forgot-password", requestForgotPassword);
-router.post("/reset-password", validatePasswordReset, resetPassword);
+router.post('/reset-password', validateRequest(resetPasswordSchema), resetPassword);
 
 module.exports = router;
