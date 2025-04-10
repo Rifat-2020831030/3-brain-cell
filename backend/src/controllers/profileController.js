4ca8c42cd30065387b81d20449f0c851d21be4b9
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-04-10 18:23:20 +0600

    Merged refactor/UD-26/dashboards into dev and resolved conflicts

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -3,6 +3,7 @@ const { sendSuccessResponse, sendErrorResponse } = require('../utils/responseHel
 const { UserDoesNotExistError, ValidationError } = require('../utils/errors');
 const jwt = require('jsonwebtoken');
 
+
 const completeRegistration = async (req, res) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return sendErrorResponse(res, 'Unauthorized: Token missing', 401);
