3c092c9cada3b929bf8abcb1870caa0f8fb10c9c
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2025-04-18 01:20:22 +0600

    feat(users): add endpoint to get ongoing disasters for all users

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
 
-//  @type {import('eslint').Linter.Config[]} 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
