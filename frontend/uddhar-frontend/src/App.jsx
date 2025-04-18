9a538a48b74d9d4a8f36d068ef8d48614f9eb108
Ben <code.hasan@protonmail.com>
2025-04-18 23:37:01 +0600

    refactor: routing relocates to seperate folder

--- frontend/uddhar-frontend/src/App.jsx
+++ frontend/uddhar-frontend/src/App.jsx
@@ -1,114 +1,71 @@
-import { Route, BrowserRouter as Router, Routes, useParams } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import ProtectedRoute from "./authentication/components/ProtectedRoutes";
 import { AuthProvider } from "./authentication/context/AuthContext";
 import ForgetPass from "./authentication/pages/ForgetPass";
 import Login from "./authentication/pages/Login";
 import Registration from "./authentication/pages/Registration";
 import Unauthorized from "./authentication/pages/UnAuthorized";
-import CoordinatorDashboard from "./coordinator/pages/CoordinatorDashboard";
-import DisasterControl from "./coordinator/pages/DisasterControl";
 import { Landing, Navbar } from "./public/Public";
-import DashboardNavbar from "./shared/components/DashboardNavbar";
-import Volunteer from "./volunteer/pages/Volunteer";
-import OrgDashboard from "./organization/pages/OrgDashboard";
-import Proptypes from "prop-types";
+import CoordinatorRoute, {
+  CreateAnEventRoute,
+} from "./routeWrapper/CoordinatorRoute";
+import OrganizationRoute from "./routeWrapper/OrganizationRoute";
+import VolunteerRoute from "./routeWrapper/VolunteerRoute";
 
 const App = () => {
-  // Wrapper to extract activeSection from route params, default to "Home"
-  const CoordinatorDashboardWrapper = ({ activeSection }) => (
-    <DashboardNavbar heading="Coordinator Dashboard">
-      <CoordinatorDashboard activeSection={activeSection || "home"} />
-    </DashboardNavbar>
-  );
-  CoordinatorDashboardWrapper.propTypes = {
-    activeSection: Proptypes.string,
-  };
-  const CoordinatorDashboardContainer = () => {
-    const { activeSection } = useParams();
-    return <CoordinatorDashboardWrapper activeSection={activeSection} />;
-  };
-
   return (
-    <>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Navbar>
-                  <Landing />
-                </Navbar>
-              }
-            />
-            <Route
-              path="/dashboard/coordinator/:activeSection?"
-              element={
-                <ProtectedRoute roles={["coordinator"]}>
-                  <CoordinatorDashboardContainer />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/sign-in"
-              element={
-                <Navbar>
-                  <Login />
-                </Navbar>
-              }
-            />
-            <Route
-              path="/sign-up"
-              element={
-                <Navbar>
-                  <Registration />
-                </Navbar>
-              }
-            />
-            <Route path="/password-recovery" element={<ForgetPass />} />
-            <Route
-              path="/create-a-event"
-              element={
-                <ProtectedRoute roles={["coordinator"]}>
-                  <DashboardNavbar heading="Coordinator Dashboard">
-                    <DisasterControl />
-                  </DashboardNavbar>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/unauthorized"
-              element={
-                <Navbar>
-                  <Unauthorized />
-                </Navbar>
-              }
-            />
-            <Route
-              path="/dashboard/volunteer"
-              element={
-                <ProtectedRoute roles={["volunteer"]}>
-                  <DashboardNavbar heading="Volunteer Dashboard">
-                      <Volunteer />
-                  </DashboardNavbar>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/organization"
-              element={
-                <ProtectedRoute roles={["organization"]}>
-                  <DashboardNavbar heading="Organization Dashboard">
-                      <OrgDashboard />
-                  </DashboardNavbar>
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Navbar>
+                <Landing />
+              </Navbar>
+            }
+          />
+          <Route
+            path="/dashboard/coordinator/:activeSection?"
+            element={<CoordinatorRoute />}
+          />
+          <Route
+            path="/sign-in"
+            element={
+              <Navbar>
+                <Login />
+              </Navbar>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <Navbar>
+                <Registration />
+              </Navbar>
+            }
+          />
+          <Route path="/password-recovery" element={<ForgetPass />} />
+          <Route path="/create-a-event" element={<CreateAnEventRoute />} />
+          <Route
+            path="/unauthorized"
+            element={
+              <Navbar>
+                <Unauthorized />
+              </Navbar>
+            }
+          />
+          <Route
+            path="/dashboard/volunteer/:activeSection?"
+            element={<VolunteerRoute />}
+          />
+          <Route
+            path="/dashboard/organization/:activeSection?"
+            element={<OrganizationRoute />}
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
--- frontend/uddhar-frontend/src/coordinator/pages/DisasterControl.jsx
+++ frontend/uddhar-frontend/src/coordinator/pages/DisasterControl.jsx
@@ -2,9 +2,9 @@ import DisasterInput from "../components/DisasterInput";
 
 const DisasterControl = () => {
   return (
-    <>
+    <div>
       <DisasterInput />
-    </>
+    </div>
   );
 };
 export default DisasterControl;
