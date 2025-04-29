2009d67c7d5a37fc7820e5d695a52cdeb33d1502
Amit Hassan Joy <sarkeramit88@gmail.com>
2025-04-30 01:05:35 +0600

    Add: profile service test suite

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,20 +1,6 @@
 module.exports = {
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'json'],
-  testMatch: ['**/tests/**/*.test.js'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/index.js',
-    '!src/config/**'
-  ],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  }
+  coverageReporters: ["lcov", "text-summary"],
+  collectCoverageFrom: ["src/**/*.js", "!src/**/*.test.js"],
+  testEnvironment: "node",
+  verbose: true,
 };
-
