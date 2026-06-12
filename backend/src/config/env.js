cb679c0a399b909d602a9039673f6c8311885afa
Rifat <108355509+Rifat-2020831030@users.noreply.github.com>
2026-06-12 18:34:20 +0600

    port and ssl config add

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -7,7 +7,9 @@ const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL
   },
   email: {
     user: process.env.EMAIL_USER,
